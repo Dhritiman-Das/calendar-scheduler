025393262af7f277fdcd1f5ee4a225883563e7fd
Dhritiman Das <iamdhritiman01@gmail.com>
2025-04-01 00:25:13 +0530

    fix: public api URL pulled from env

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3333/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3333/v1",
   headers: {
     "Content-Type": "application/json",
   },
